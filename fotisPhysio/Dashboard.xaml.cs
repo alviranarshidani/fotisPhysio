cba026056b756d1f1f9bdd5498c721d42165b496
alviranarshidani <200394236@student.georgianc.on.ca>
2019-06-16 01:50:38 -0400

    dashboard page

---
+++ fotisPhysio/Dashboard.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace fotisPhysio
+{
+    public partial class Dashboard : ContentPage
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            
+            
+        }
+    }
+}
