66b5f11553bc3e3594e868134d419c5943822c6e
priyanka199309@gmail.com <gargpriyanka199309@gmail.com>
2019-06-17 14:07:51 -0400

    * MainPage.xaml.cs:
    * MainPage.xaml: Home page added

--- fotisPhysio/MainPage.xaml.cs
+++ fotisPhysio/MainPage.xaml.cs
@@ -31,9 +31,11 @@ namespace fotisPhysio
             IsPresented = false;
         }
 
-        void Handle_Clicked_5(object sender, System.EventArgs e)
+        void Home(object sender, System.EventArgs e)
         {
+            Detail = new NavigationPage(new MyHomePage());
 
+            IsPresented = false;
 
         }
 
